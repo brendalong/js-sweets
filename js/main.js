bd7973444e2f72c5f2c53a50a00a6400fd068b40
Brenda Long <brenda@studio6thave.com>
2018-01-24 06:07:31 -0600

    starter files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("ld main.js");
+
+function makeSweet(name, kind, flavor, topping, topcolor){
+    name = name;
+    kind = kind;
+    flavor = flavor;
+    topping = topping;
+    topcolor = topcolor;
+};
+
+inventory.push(new makeSweet("Christmas Tree", "cake", "white", "none", "green"));
